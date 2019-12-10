94a55838bebd922e11ffb4ce367fa71eb8df1503
． ． <m151109@ishikawa.kosen-ac.jp>
2019-12-10 17:35:59 +0900

    Constructing LineChart

---
+++ GA_AirfoilOptimizationTool2D/Chart/AxisStyle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_AirfoilOptimizationTool2D.Chart
+{
+    class AxisStyle
+    {
+        private double min = 0.0;
+        public double Min
+        {
+            get => min;
+            set
+            {
+
+            }
+        }
+    }
+}

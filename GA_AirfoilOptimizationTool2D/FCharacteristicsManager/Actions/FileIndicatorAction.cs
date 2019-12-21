f5404f509c68a561b46de8e7520a77cb7dc7fe26
． ． <m151109@ishikawa.kosen-ac.jp>
2019-12-21 19:11:55 +0900

    FileIndicator

---
+++ GA_AirfoilOptimizationTool2D/FCharacteristicsManager/Actions/FileIndicatorAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace GA_AirfoilOptimizationTool2D.FCharacteristicsManager.Actions
+{
+    class FileIndicatorAction : TriggerAction<FrameworkElement>
+    {
+        public FileIndicatorAction() { }
+        protected override void Invoke(object parameter)
+        {
+            
+        }
+    }
+}
